59abd1d9a9327a2ef4ce7816238dd057c40da15f
Dennis Kimathi <kimathidennis38@gmail.com>
2022-09-08 23:57:54 +0300

    deploy: first deployment

--- inventory-tracker-backend/db/seeds.rb
+++ inventory-tracker-backend/db/seeds.rb
@@ -25,15 +25,15 @@ puts "Creating Printers..."
 # * and a printer belongs to an enterprise *
 # ***************************************************************
 # Create printers Here
-laserjet = Printer.create(model: "Laserjet MFP M283fdw", lease_terms: 2, payment_per_month: 2000, purchase_price: 94000, condition: "Good", enterprise_id: ufanisi.id , manufacturer_id: hp.manufacturers.company_name )
+laserjet = Printer.create(model: "Laserjet MFP M283fdw", lease_terms: 2, payment_per_month: 2000, purchase_price: 94000, condition: "Good", enterprise_id: ufanisi.id , manufacturer_id: hp.id )
 
-epsonl850 = Printer.create(model: "Epson L850 ", lease_terms: 3, payment_per_month: 4000, purchase_price: 80000, condition: "Good", enterprise_id: cooperative.id , manufacturer_id: epson.company_name)
+epsonl850 = Printer.create(model: "Epson L850 ", lease_terms: 3, payment_per_month: 4000, purchase_price: 80000, condition: "Good", enterprise_id: cooperative.id , manufacturer_id: epson.id)
 
-km2560 = Printer.create(model: "Kyocera KM- 2560", lease_terms: 5, payment_per_month: 1500, purchase_price: 90000, condition: "Good", enterprise_id: cannonical.id , manufacturer_id: kyocera.company_name )
+km2560 = Printer.create(model: "Kyocera KM- 2560", lease_terms: 5, payment_per_month: 1500, purchase_price: 90000, condition: "Good", enterprise_id: cannonical.id , manufacturer_id: kyocera.id )
 
-canon = Printer.create(model: "Canon G6040 duplex", lease_terms: 2, payment_per_month: 2500, purchase_price: 134900, condition: "Good", enterprise_id: mercycorps.id , manufacturer_id: panasonic.company_name)
+canon = Printer.create(model: "Canon G6040 duplex", lease_terms: 2, payment_per_month: 2500, purchase_price: 134900, condition: "Good", enterprise_id: mercycorps.id , manufacturer_id: panasonic.id)
 
-dellinkjet = Printer.create(model: "All in one inkjet", lease_terms: 4, payment_per_month: 4000, purchase_price: 124000, condition: "Good", enterprise_id: knhcr.id , manufacturer_id: dell.company_name)
+dellinkjet = Printer.create(model: "All in one inkjet", lease_terms: 4, payment_per_month: 4000, purchase_price: 124000, condition: "Good", enterprise_id: knhcr.id , manufacturer_id: dell.id)
 
 
 puts "Creating clients..."
@@ -44,15 +44,15 @@ jordan = Client.create(name: "Jordan Peterson", location: "Kimberley Way 105", p
 joel = Client.create(name: "Joel Matip", location: "123 Main St.", phone: 254734435512, email: "jmatip@hotmail.com")
 
 puts "Creating computers..."
-dell3500 = Computer.create(model: "Dell Vostro 3500 ", core: "i5", disk_space:"256 SSD", ram: "8GB", lease_terms: 2, payment_per_month: 2000, purchase_price: 6000, condition: "New" , client_id: dennis.name)
+dell3500 = Computer.create(model: "Dell Vostro 3500 ", core: "i5", disk_space:"256 SSD", ram: "8GB", lease_terms: 2, payment_per_month: 2000, purchase_price: 6000, condition: "New" , client_id: dennis.id)
 
-hp = Computer.create(model: "HP ELITEBOOK 840 G1", core: "i5", disk_space:"500GB HDD", ram: "8GB", lease_terms: 3, payment_per_month: 4000, purchase_price: 45000, condition: "New" , client_id: charles.name)
+hp = Computer.create(model: "HP ELITEBOOK 840 G1", core: "i5", disk_space:"500GB HDD", ram: "8GB", lease_terms: 3, payment_per_month: 4000, purchase_price: 45000, condition: "New" , client_id: charles.id)
 
-macbook = Computer.create(model: "Apple Macbook Pro A1708", core: "i5-6360U", disk_space:"256 SSD", ram: "8GB", lease_terms: 3, payment_per_month: 5000, purchase_price: 90000, condition: "Refurbished" , client_id: luka.name)
+macbook = Computer.create(model: "Apple Macbook Pro A1708", core: "i5-6360U", disk_space:"256 SSD", ram: "8GB", lease_terms: 3, payment_per_month: 5000, purchase_price: 90000, condition: "Refurbished" , client_id: luka.id)
 
-toshiba = Computer.create(model: "Toshiba Tecra x40-D", core: "i5-7300U", disk_space:"256 SSD", ram: "8GB", lease_terms: 4, payment_per_month: 4350, purchase_price: 34500, condition: "New" , client_id: jordan.name)
+toshiba = Computer.create(model: "Toshiba Tecra x40-D", core: "i5-7300U", disk_space:"256 SSD", ram: "8GB", lease_terms: 4, payment_per_month: 4350, purchase_price: 34500, condition: "New" , client_id: jordan.id)
 
-lenovo = Computer.create(model: "Lenovo V14", core: "Intel Celeron", disk_space:"1TB", ram: "4GB", lease_terms: 2, payment_per_month: 1500, purchase_price: 4, condition: "Good" , client_id: joel.name)
+lenovo = Computer.create(model: "Lenovo V14", core: "Intel Celeron", disk_space:"1TB", ram: "4GB", lease_terms: 2, payment_per_month: 1500, purchase_price: 4, condition: "Good" , client_id: joel.id)
 
 
 
